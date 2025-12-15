3b9db02bd0bc108512edfc0adf5b3fa6b13fb6cd
Trevor <valentinosolischang@gmail.com>
2025-12-14 23:29:31 -0500

    Remove unused import for ExternalDocumentation in OpenApiConfiguration

--- src/main/java/ia/roadzen/platform/u20231b475/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
+++ src/main/java/ia/roadzen/platform/u20231b475/shared/infrastructure/documentation/openapi/configuration/OpenApiConfiguration.java
@@ -1,6 +1,5 @@
 package ia.roadzen.platform.u20231b475.shared.infrastructure.documentation.openapi.configuration;
 
-import io.swagger.v3.oas.models.ExternalDocumentation;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
